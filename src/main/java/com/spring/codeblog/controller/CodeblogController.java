12594a11dd71d5d00e4c80bd13c3fe7b80bca95f
danielsouzapereira <pereirasouzadaniel@gmail.com>
2022-07-26 16:00:57 -0300

    Implementação do metodo get postDetails na Classe CodeblogController, assim como a criação e implementação da pagina postDetails.html

--- src/main/java/com/spring/codeblog/controller/CodeblogController.java
+++ src/main/java/com/spring/codeblog/controller/CodeblogController.java
@@ -5,6 +5,7 @@ import com.spring.codeblog.service.CodeblogService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -15,16 +16,26 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class CodeblogController {
+
     @Autowired
     CodeblogService codeblogService;
-    
-    @RequestMapping(value= "/posts", method = RequestMethod.GET)    
-    public ModelAndView getPosts(){
+
+    @RequestMapping(value = "/posts", method = RequestMethod.GET)
+    public ModelAndView getPosts() {
         ModelAndView mv = new ModelAndView("posts");
         List<Post> posts = codeblogService.findAll();
         mv.addObject("posts", posts);
         return mv;
-        
-    }    
-    
+    }
+
+
+    @RequestMapping(value = "/posts/{id}", method = RequestMethod.GET)
+    public ModelAndView getPostDetails(@PathVariable("id")long id) {
+                                           /*nome da pagina html*/
+        ModelAndView mv = new ModelAndView("postDetails");
+        Post post = codeblogService.findById(id);
+        mv.addObject("post", post);
+        return mv;
+    }
+
 }
